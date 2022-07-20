a822e30b0f36bf9efb54aba2b58b2f78741521a0
Emmanuel <emmanuel.navis@tigeranalytics.com>
2022-07-21 00:23:31 +0530

    mlflow-housing

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import ElasticNet
+from sklearn.svm import SVR
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.model_selection import StratifiedShuffleSplit, train_test_split
 
@@ -38,7 +39,7 @@ def load_data(data_path):
     imputer.fit(housing_num)
     X = imputer.transform(housing_num)
 
-    housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=housing.index)
+    housing_tr = pd.DataFrame(X, columns=housing_num.columns, index=data.index)
     housing_tr["rooms_per_household"] = (
         housing_tr["total_rooms"] / housing_tr["households"]
     )
@@ -78,24 +79,24 @@ if __name__ == "__main__":
     data_path = "data/housing.csv"
     train_x, train_y, test_x, test_y = load_data(data_path)
 
-    alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 0.5
-    l1_ratio = float(sys.argv[2]) if len(sys.argv) > 2 else 0.5
+    c = float(sys.argv[1]) if len(sys.argv) > 1 else 30000.0
+    k = str(sys.argv[2]) if len(sys.argv) > 2 else 'linear'
 
     with mlflow.start_run():
-        lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=42)
+        lr = SVR(C=c, kernel=k)
         lr.fit(train_x, train_y)
 
         predicted_qualities = lr.predict(test_x)
 
         (rmse, mae, r2) = eval_metrics(test_y, predicted_qualities)
 
-        print("Elasticnet model (alpha=%f, l1_ratio=%f):" % (alpha, l1_ratio))
+        print("SVR model (C=%f, Kernal=%f):" % (c, k))
         print("  RMSE: %s" % rmse)
         print("  MAE: %s" % mae)
         print("  R2: %s" % r2)
 
-        mlflow.log_param("alpha", alpha)
-        mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_param("C", c)
+        mlflow.log_param("Kernal", k)
         mlflow.log_metric("rmse", rmse)
         mlflow.log_metric("r2", r2)
         mlflow.log_metric("mae", mae)
